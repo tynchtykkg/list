15f10b267601a660de9310179a599293b3b7f65b
tynchtykkg <tynchtyk.estebesuulu@gmail.com>
2023-11-02 17:06:22 +0600

    second commit

--- bin/tasks/task_two.dart
+++ bin/tasks/task_two.dart
@@ -1,14 +1,14 @@
 void main(){
-  List<int> men = [3, 12, 43, 1, 25, 6, 5, 7];
-  List<int> women = [55, 11, 23, 56, 78, 1, 9];
-  men.addAll(women);
-  print(men);
+  List<int> man = [3, 12, 43, 1, 25, 6, 5, 7];
+  List<int> woman = [55, 11, 23, 56, 78, 1, 9];
+  man.addAll(woman);
+  print(man);
   
 
   //or
 
-  List<int> men2 = [3, 12, 43, 1, 25, 6, 5, 7];
-  List<int> women2 = [55, 11, 23, 56, 78, 1, 9];
-  List<int> people = [...men2, ...women2];
+  List<int> men = [3, 12, 43, 1, 25, 6, 5, 7];
+  List<int> women = [55, 11, 23, 56, 78, 1, 9];
+  List<int> people = [...men, ...women];
   print(people);
 }
